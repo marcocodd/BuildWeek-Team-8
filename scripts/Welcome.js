cbddd9e2e88065b824792b8403149b1caefe8bb0
GiulioChiarenza <giulio.chiarenza@outlook.it>
2023-12-12 10:06:28 +0100

    proceedButton

--- scripts/Welcome.js
+++ scripts/Welcome.js
@@ -1,10 +1,15 @@
 
 const checkBox= document.getElementById('accept')
-const proceedButton= document.getElementById('proceedButton')
+const proceedButton= document.getElementById('proceed-button')
 checkBox.addEventListener('change', function() {
     if(this.checked) {
         proceedButton.disabled === false
     } else {
         proceedButton.disabled === true
     }
+})
+proceedButton.addEventListener('click', function(){
+    if(checkBox.checked){
+        Window.location.href = 'Questionpage.html'
+    } else {alert('per procedere devi aver accettato i termini e le condizioni')}
 })
