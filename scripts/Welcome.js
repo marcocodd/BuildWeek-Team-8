ac57b6d4a172273e61071157b2b294263b67000e
Ermias De Angeli <deangeliermias@gmail.com>
2023-12-14 16:01:46 +0100

    Welcome.Javascript

--- scripts/Welcome.js
+++ scripts/Welcome.js
@@ -20,13 +20,22 @@ proceedButton.addEventListener('click', function () {
 
 const selectDifficulty = function() {
     const difficulty = document.getElementById("difficulty").value;
-    const numQuestions = parseInt(document.getElementById("numberQuestions").value);
-    if (numQuestions < 5 || numQuestions > 30) {
+    const numberQuestions = parseInt(document.getElementById("numberQuestions").value);
+    if (numberQuestions < 5 || numberQuestions > 30) {
         alert("Number must be between 5 and 30.");
         return;
     }
     console.log("Difficulty selected:", difficulty);
-    console.log("Number questions:", numQuestions);
+    console.log("Number questions:", numberQuestions);
+
+	const difficultyObject = {
+        difficulty: difficulty,
+        numbQuestions: numberQuestions
+    }
+
+	const JSONString = JSON.stringify(difficultyObject);
+
+	localStorage.setItem('testOptions', JSONString);
 
 	window.location.href = "Welcomepage.html" + "Questionpage.html"
 
